201b438846578d59d776e1fcea309da1c48d0dab
Raúl Ibarra <raul.ibarra004@gmail.com>
2025-07-21 15:46:07 -0700

    chore: Add enviroment file to gitignore.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,10 @@
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "AIzaSyBkPkQr7Ty5d2wK30hzlDRjpRJwVs61IO8",
-    authDomain: "to-do-app-with-firebase-8abb9.firebaseapp.com",
-    projectId: "to-do-app-with-firebase-8abb9",
-    storageBucket: "to-do-app-with-firebase-8abb9.firebasestorage.app",
-    messagingSenderId: "247613091737",
-    appId: "1:247613091737:web:7754b3df49335d03766d08",
-    measurementId: "G-B7RTSS9DH9"
-  }
-
+  apiKey: "AIzaSyBkPkQr7Ty5d2wK30hzlDRjpRJwVs61IO8",
+  authDomain: "to-do-app-with-firebase-8abb9.firebaseapp.com",
+  projectId: "to-do-app-with-firebase-8abb9",
+  storageBucket: "to-do-app-with-firebase-8abb9.firebasestorage.app",
+  messagingSenderId: "247613091737",
+  appId: "1:247613091737:web:c36438342e686c51766d08",
+  measurementId: "G-7KC359MEEW"
 };
+
