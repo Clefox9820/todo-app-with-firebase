aa9c07683fa56763218ca0a614d838845f47f235
Raúl Ibarra <raul.ibarra004@gmail.com>
2025-07-21 16:36:41 -0700

    fix(main.ts): fix ionic styles not showing.
    The ionic styles were not showing because in main.ts there is not ionic provider.

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,28 @@
 // src/main.ts
-import { bootstrapApplication }   from '@angular/platform-browser';
-import { provideRouter }          from '@angular/router';
-import { initializeApp }          from 'firebase/app';
-import { provideFirebaseApp }     from '@angular/fire/app';
-import { getAuth, provideAuth }   from '@angular/fire/auth';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { initializeApp } from 'firebase/app';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { AppComponent }           from './app/app.component';
-import { routes }                 from './app/app.routes';
-import { environment }            from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+import { environment } from './environments/environment';
+
+// IMPORTS DE IONIC
+import { provideIonicAngular, IonicRouteStrategy } from '@ionic/angular/standalone';
+import { RouteReuseStrategy } from '@angular/router';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    // Router, Firebase ...
     provideRouter(routes),
-
-    // 1) Inicializa la instancia de FirebaseApp
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-
-    // 2) Provee el servicio de Auth
     provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
 
-    // 3) Provee el servicio de Firestore
-    provideFirestore(() => getFirestore())
+    // PROVEEDORES DE IONIC
+    provideIonicAngular(),
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ]
-})
-.catch(err => console.error('Error bootstrapping application:', err));
+}).catch(err => console.error(err));
