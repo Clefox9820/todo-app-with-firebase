8525b328f6cc900bd1436cd596781b85e0f89083
Raúl Ibarra <raul.ibarra004@gmail.com>
2025-07-23 15:45:05 -0700

    feat(header): Add a header component.
    Added a header component to test the logout logic.

--- src/app/Components/AuxiliarUI/header/header.component.ts
+++ src/app/Components/AuxiliarUI/header/header.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonButton, IonIcon, IonButtons } from "@ionic/angular/standalone";
+import { AuthService } from 'src/app/Services/auth.service';
+import { LoginFormData } from '../../Auth/form/login/login-form.component';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
+  imports: [IonButtons, IonIcon, IonTitle, IonToolbar, IonHeader, IonButton, AsyncPipe],
   standalone: true,
 })
-export class HeaderComponent  implements OnInit {
+export class HeaderComponent implements OnInit {
+  private router = inject(Router);
+  isAuthenticated$: Observable<boolean>;
+  authService = inject(AuthService);
 
-  constructor() { }
+  error: string | null = null;
 
-  ngOnInit() {}
+
+  constructor() {
+    this.isAuthenticated$ = this.authService.isAuthenticated();
+  }
+
+
+  ngOnInit() { }
+  
+  async onLogout() {
+    this.error = null;
+    try {
+      await this.authService.logout();
+      await this.router.navigateByUrl('/login');
+    } catch (err: any) {
+      this.error = err.message;
+    }
+  }
 
 }
