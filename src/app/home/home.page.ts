3db7241a13d5fe7d16cd7783bd1ca28ce1464f92
Raúl Ibarra <raul.ibarra004@gmail.com>
2025-07-23 15:48:54 -0700

    refactor(home.page): add the header component to home page template.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { LoginComponent } from "../Components/Auth/login/login.component";
+import { HeaderComponent } from "../components/AuxiliarUI/header/header.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonContent, HeaderComponent],
 })
 export class HomePage {
   constructor() {}
