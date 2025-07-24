2de008aee490546f51aaf8f867907ef100a86261
Raúl Ibarra <raul.ibarra004@gmail.com>
2025-07-23 18:32:27 -0700

    feat(home.page): Add the searchBar and filter components.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { HeaderComponent } from "../components/AuxiliarUI/header/header.component";
+import { searchBarComponent } from '../components/searchBar/searchBar.component';
+import { FilterComponent } from '../components/FiltersAndTags/filter/filter.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonContent, HeaderComponent],
+  imports: [IonContent, HeaderComponent, searchBarComponent, FilterComponent],
 })
 export class HomePage {
   constructor() {}
