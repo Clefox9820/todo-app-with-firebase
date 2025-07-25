0074cc61c842bc0c8b0967fb8c0fd6f48c8661c7
Raúl Ibarra <raul.ibarra004@gmail.com>
2025-07-25 16:58:28 -0700

    refactor(task-item): refactor to receive the tasklist information from tasklist component.

--- src/app/Components/Tasks/task-item/task-item.component.ts
+++ src/app/Components/Tasks/task-item/task-item.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonLabel, IonItem, IonChip, IonCheckbox, IonRow, IonGrid, IonCol } from '@ionic/angular/standalone';
+import { TodoTask } from 'src/app/interfaces/task.interface';
 
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
   styleUrls: ['./task-item.component.scss'],
+  imports: [IonCol, IonGrid, IonRow, IonItem, IonLabel, IonChip, FormsModule, IonCheckbox],
   standalone: true,
 })
-export class TaskItemComponent  implements OnInit {
+export class TaskItemComponent implements OnInit {
+    category = ['Personal', 'Trabajo', 'Hogar', 'Otros'];
+
+
+  @Input() taskList: TodoTask = {} as TodoTask
+
+
+  onToggleDone(event: any) {
+    this.taskList.done = event.detail.checked
+  }
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
