43edcd492fbd48a0f617d0843688060d1ffd4444
Raúl Ibarra <raul.ibarra004@gmail.com>
2025-07-31 11:33:41 -0700

    refactor(home.page)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,15 +1,52 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { HeaderComponent } from "../components/AuxiliarUI/header/header.component";
-import { searchBarComponent } from '../components/searchBar/searchBar.component';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
+import { TaskListComponent } from '../../app/components/Tasks/task-list/task-list.component';
+import { TodoTask } from '../../app/interfaces/task.interface';
 import { FilterComponent } from '../components/FiltersAndTags/filter/filter.component';
+import { HeaderComponent } from "../components/AuxiliarUI/header/header.component";
+import { searchBarComponent } from "../components/searchBar/searchBar.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonContent, HeaderComponent, searchBarComponent, FilterComponent],
+  standalone: true,
+  imports: [
+    CommonModule,
+    IonHeader,
+    IonContent,
+    FilterComponent,
+    TaskListComponent,
+    HeaderComponent,
+    searchBarComponent
+],
 })
 export class HomePage {
-  constructor() {}
+  // Signal para las tareas (opcional, TaskListComponent tiene sus propias tareas por defecto)
+  tasks = signal<TodoTask[]>([]);
+
+  constructor() {
+    // Si quieres pasar tareas específicas desde el padre, puedes hacerlo aquí
+    // this.loadTasksFromParent();
+  }
+
+  private loadTasksFromParent() {
+    // Ejemplo de cómo pasar tareas desde el componente padre
+    this.tasks.set([
+      {
+        id: '1',
+        ownerId: 'user1',
+        title: 'Tarea desde HomePage',
+        description: 'Esta tarea viene del componente padre',
+        categories: [0, 1],
+        done: false
+      }
+    ]);
+  }
 }
