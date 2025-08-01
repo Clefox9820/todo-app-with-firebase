0338014e28f3fc2efcbd49525dd88ce7d69c7f2f
Raúl Ibarra <raul.ibarra004@gmail.com>
2025-08-01 14:50:39 -0700

    chore(home.page): add components to home template.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonCheckbox, IonFooter, IonFab, IonFabButton, IonIcon, IonButton } from '@ionic/angular/standalone';
 import { HeaderComponent } from "../components/AuxiliarUI/header/header.component";
+import { FilterComponent } from '../components/FiltersAndTags/filter/filter.component';
+import { TaskListComponent } from "../components/Tasks/task-list/task-list.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonContent, HeaderComponent],
+  imports: [IonIcon, IonFabButton, IonFab, IonFooter, IonCheckbox, IonContent, HeaderComponent, IonSearchbar, FilterComponent, IonHeader, IonToolbar, IonTitle, TaskListComponent, IonButton],
 })
 export class HomePage {
   constructor() {}
