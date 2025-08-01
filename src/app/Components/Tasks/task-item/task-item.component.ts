88f083871637d5745f395903078e709f46bac01e
Raúl Ibarra <raul.ibarra004@gmail.com>
2025-08-01 14:49:37 -0700

    feat(task.item.component): Add a task item component prototype.

--- src/app/Components/Tasks/task-item/task-item.component.ts
+++ src/app/Components/Tasks/task-item/task-item.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, input, OnInit, signal } from '@angular/core';
+import { TodoTask } from 'src/app/interfaces/task.interface';
+import { IonItem, IonLabel, IonChip, IonCheckbox, IonButton, IonIcon, IonButtons } from "@ionic/angular/standalone";
 
 @Component({
-  selector: 'app-task-item',
+  selector: 'task-item',
   templateUrl: './task-item.component.html',
   styleUrls: ['./task-item.component.scss'],
+  imports: [IonButtons, IonIcon, IonButton, IonCheckbox, IonChip, IonLabel, IonItem],
   standalone: true,
 })
-export class TaskItemComponent  implements OnInit {
+export class TaskItemComponent implements OnInit {
+
+  //Crear signal task con input
+  taskItem = input<TodoTask>();
+
+
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
 }
