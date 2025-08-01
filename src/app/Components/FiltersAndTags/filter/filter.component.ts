7af45fde97674be03eb5f238ab51987dcdb2699c
Raúl Ibarra <raul.ibarra004@gmail.com>
2025-08-01 14:48:03 -0700

    feat(filter.component): Add a filter component without functionality.

--- src/app/Components/FiltersAndTags/filter/filter.component.ts
+++ src/app/Components/FiltersAndTags/filter/filter.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { IonList, IonItem, IonSelect, IonSelectOption, IonCheckbox, IonSearchbar } from "@ionic/angular/standalone";
 
 @Component({
-  selector: 'app-filter',
+  selector: 'filter',
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.scss'],
+  imports: [IonSearchbar, IonCheckbox, IonList, IonItem, IonSelect, IonSelectOption],
   standalone: true,
 })
 export class FilterComponent  implements OnInit {
