c2c2dc036674f58804c7292d6344402a061b4692
Raúl Ibarra <raul.ibarra004@gmail.com>
2025-08-01 21:20:33 -0700

    chore(home.page): add the logic that shares the status from "select all" checkbox status between two components.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, model } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonCheckbox, IonFooter, IonFab, IonFabButton, IonIcon, IonButton } from '@ionic/angular/standalone';
 import { HeaderComponent } from "../components/AuxiliarUI/header/header.component";
 import { FilterComponent } from '../components/FiltersAndTags/filter/filter.component';
@@ -11,5 +11,9 @@ import { TaskListComponent } from "../components/Tasks/task-list/task-list.compo
   imports: [IonIcon, IonFabButton, IonFab, IonFooter, IonCheckbox, IonContent, HeaderComponent, IonSearchbar, FilterComponent, IonHeader, IonToolbar, IonTitle, TaskListComponent, IonButton],
 })
 export class HomePage {
+  
+selectAll = model(false)
+
+
   constructor() {}
 }
