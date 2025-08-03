e5fa4e589bc94bd42f0833721f291d408ab6bc43
Raúl Ibarra <raul.ibarra004@gmail.com>
2025-08-02 19:49:30 -0700

    feat(task.list): add a addHarcodeTaksButton.

--- src/app/Components/Tasks/task-list/task-list.component.ts
+++ src/app/Components/Tasks/task-list/task-list.component.ts
@@ -1,14 +1,17 @@
-import { Component, model, OnInit } from '@angular/core';
-import { IonList, IonItem } from "@ionic/angular/standalone";
+import { Component, inject, model, OnInit } from '@angular/core';
+import { IonList, IonItem, IonRefresher, IonRefresherContent, RefresherCustomEvent, IonButton, IonIcon } from "@ionic/angular/standalone";
 import { TodoTask } from 'src/app/interfaces/task.interface';
 import { TaskItemComponent } from "../task-item/task-item.component";
 import { TaskService } from '../../../Services/task.service';
+import { Timestamp } from '@angular/fire/firestore';
+import { Auth } from '@angular/fire/auth';
+import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
   selector: 'task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.scss'],
-  imports: [IonList, TaskItemComponent],
+  imports: [IonIcon, IonButton, IonRefresherContent, IonRefresher, IonList, TaskItemComponent],
   standalone: true,
 })
 export class TaskListComponent implements OnInit {
@@ -18,7 +21,8 @@ export class TaskListComponent implements OnInit {
   filter = 'all';
   checked = model<boolean>(false);
 
-  constructor(private taskService: TaskService) {}
+  constructor(private taskService: TaskService) { }
+  auth: AuthService = inject(AuthService);
 
   ngOnInit() {
     this.taskService.getTasks().subscribe(tasks => {
@@ -26,6 +30,40 @@ export class TaskListComponent implements OnInit {
       // this.filterTasks();
     });
   }
+
+
+  get() {
+    this.taskService.getTasks().subscribe(tasks => {
+      this.allTasks = tasks;
+      // this.filterTasks();
+    });
+  }
+
+  handleRefresh(event: RefresherCustomEvent) {
+    setTimeout(() => {
+      // Any calls to load data go here
+      event.target.complete();
+    }, 2000);
+  }
+
+  async addHardcodedTask() {
+  try {
+    const taskData = {
+      title: 'Tarea de prueba',
+      description: 'Esta es una tarea hardcodeada para testing',
+      completed: false,
+      deadline: Timestamp.now(), // Esta propiedad faltaba
+      category: [1,2,33] // ajusta según tu tipo
+    };
+
+    const taskId = await this.taskService.addTask(taskData);
+    console.log('Tarea creada con ID:', taskId);
+
+    // Opcional: mostrar mensaje de éxito o recargar lista
+  } catch (error) {
+    console.error('Error al crear tarea:', error);
+  }
+}
 
   // filterTasks() {
   //   if (this.filter === 'pending') {
