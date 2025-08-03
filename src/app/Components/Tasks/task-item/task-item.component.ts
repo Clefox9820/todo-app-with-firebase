2fceedb54bc3b05aa86e2c60024b014298124062
Raúl Ibarra <raul.ibarra004@gmail.com>
2025-08-02 19:48:55 -0700

    feat(task.item): add a delete button.

--- src/app/Components/Tasks/task-item/task-item.component.ts
+++ src/app/Components/Tasks/task-item/task-item.component.ts
@@ -2,6 +2,7 @@ import { Component, input, model, OnInit, output, signal } from '@angular/core';
 import { TodoTask } from 'src/app/interfaces/task.interface';
 import { IonItem, IonLabel, IonChip, IonCheckbox, IonButton, IonIcon, IonButtons } from "@ionic/angular/standalone";
 import { FormsModule } from '@angular/forms';
+import { TaskService } from '../../../Services/task.service';
 
 @Component({
   selector: 'task-item',
@@ -28,9 +29,19 @@ export class TaskItemComponent implements OnInit {
     this.checked.set(value);
   }
 
+  onDelete() {
+    this.taskService.deleteTask(this.taskItem()?.id!);
+    console.log('Tarea eliminandose');
+    // Activar restorTask despeus de 5 segundos
+    setTimeout(() => {
+      this.taskService.restoreTask(this.taskItem()?.id!);
+      console.log('Tarea restaurada');
+    }, 5000);
+  }
 
 
-  constructor() { }
+
+  constructor(private taskService: TaskService) { }
 
   ngOnInit() {
   }
